a7b3900e72228c61d044fde81897872317619672
sarathdesktop <62090968+sarathdesktop@users.noreply.github.com>
2020-10-25 19:05:51 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,18 @@
+# >heroku create convertexo
+
+
+#https://convertexo.herokuapp.com/ | https://git.heroku.com/convertexo.git
+
+# https://convertexo.herokuapp.com/
+
+# virtual\Scripts\activate
+# python main.py
+
+# pyinstaller -w -F --add-data "templates;templates" --add-data "static;static" --add-data "tmp;tmp" main.py
+
+# >FLASK_ENV=development
+
+# mkdir tmp
+# touch tmp/.keep
+# git add tmp/.keep
+# git commit -m "Add tmp directory to app repository."
